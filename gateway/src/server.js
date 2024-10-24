c1390861bdd321ea568e383de58e657d4bbbc0cf
viko42 <vlancien.paris@gmail.com>
2024-10-24 23:57:29 +0100

    chore: demo html website

--- gateway/src/server.js
+++ gateway/src/server.js
@@ -1,7 +1,7 @@
 // server.js
-const express = require('express');
-const { ethers } = require('ethers');
-const NodeCache = require('node-cache');
+const express = require("express");
+const { ethers } = require("ethers");
+const NodeCache = require("node-cache");
 
 const app = express();
 
@@ -13,9 +13,9 @@ let helia;
 let fs;
 
 async function initializeHelia() {
-  const { createHelia } = await import('helia');
-  const { unixfs } = await import('@helia/unixfs');
-  
+  const { createHelia } = await import("helia");
+  const { unixfs } = await import("@helia/unixfs");
+
   helia = await createHelia();
   fs = unixfs(helia);
 
@@ -37,201 +37,233 @@ async function startServer() {
 // Your Ethereum provider
 const rpcUrl = process.env.RPC_URL;
 if (!rpcUrl) {
-  throw new Error('RPC_URL environment variable is not set');
+  throw new Error("RPC_URL environment variable is not set");
 }
 const provider = new ethers.JsonRpcProvider(rpcUrl);
 
 // Your Ethereum contract
 const contractAddress = process.env.CONTRACT_ADDRESS;
+const contractRNSUnifiedAddress = process.env.CONTRACT_RNS_UNIFIED_ADDRESS;
 if (!contractAddress) {
-  throw new Error('CONTRACT_ADDRESS environment variable is not set');
+  throw new Error("CONTRACT_ADDRESS environment variable is not set");
 }
 const contractABI = [
-    {
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "_defaultIpfsLink",
-                "type": "string"
-            },
-            {
-                "internalType": "address",
-                "name": "_nftContractAddress",
-                "type": "address"
-            }
-        ],
-        "stateMutability": "nonpayable",
-        "type": "constructor"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "id",
-                "type": "string"
-            }
-        ],
-        "name": "getIpfsLink",
-        "outputs": [
-            {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "defaultIpfsLink",
-        "outputs": [
-            {
-                "internalType": "string",
-                "name": "",
-                "type": "string"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "nftContract",
-        "outputs": [
-            {
-                "internalType": "contract RNSChecker",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "id",
-                "type": "string"
-            },
-            {
-                "internalType": "string",
-                "name": "ipfsLink",
-                "type": "string"
-            }
-        ],
-        "name": "updateRnsLink",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "string",
-                "name": "_newDefaultLink",
-                "type": "string"
-            }
-        ],
-        "name": "updateDefaultIpfsLink",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    }
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_defaultIpfsLink",
+        type: "string",
+      },
+      {
+        internalType: "address",
+        name: "_nftContractAddress",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "id",
+        type: "string",
+      },
+    ],
+    name: "getIpfsLink",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "defaultIpfsLink",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "nftContract",
+    outputs: [
+      {
+        internalType: "contract RNSChecker",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "id",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "ipfsLink",
+        type: "string",
+      },
+    ],
+    name: "updateRnsLink",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_newDefaultLink",
+        type: "string",
+      },
+    ],
+    name: "updateDefaultIpfsLink",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+const contractRNSUnifiedABI = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "str",
+        type: "string",
+      },
+    ],
+    name: "namehash",
+    outputs: [
+      {
+        internalType: "bytes32",
+        name: "hashed",
+        type: "bytes32",
+      },
+    ],
+    stateMutability: "pure",
+    type: "function",
+  },
 ];
 
 // Create contract instance
 const contract = new ethers.Contract(contractAddress, contractABI, provider);
+const contractRNSUnified = new ethers.Contract(
+  contractRNSUnifiedAddress,
+  contractRNSUnifiedABI,
+  provider
+);
 
+async function getRNSUnifiedLink(rns) {
+  try {
+    const namehash = await contractRNSUnified.namehash(rns);
+
+    // Convert the result to decimal
+    const namehashDecimal = BigInt(namehash).toString(10);
+    return namehashDecimal;
+  } catch (error) {
+    console.error(`Error for RNS ${rns}:`, error);
+    throw error;
+  }
+}
 // Helper function to get IPFS content using ID
 async function getIpfsContentById(id) {
-    try {
-        // Check cache first
-        const cached = cache.get(id);
-        if (cached) {
-            console.log(`Cache hit for ${id}`);
-            return cached;
-        }
-
-        console.log(`[0] Fetching IPFS hash for ID ${id}`);
-        const ipfsHash = await contract.getIpfsLink(id); // Call contract function
-
-        if (!ipfsHash) {
-            throw new Error('No IPFS hash found');
-        }
-
-        if (!fs) {
-            throw new Error('IPFS system not initialized');
-        }
-
-        // Get content from IPFS using Helia
-        const decoder = new TextDecoder()
-        let content = ''
-        for await (const chunk of fs.cat(ipfsHash)) {
-            content += decoder.decode(chunk, { stream: true })
-        }
-
-        // Save to cache
-        cache.set(id, content);
-
-        return content;
-    } catch (error) {
-        console.error(`Error for ID ${id}:`, error);
-        throw error;
+  try {
+    // Check cache first
+    const cached = cache.get(id);
+    if (cached) {
+      return cached;
     }
+    const ipfsHash = await contract.getIpfsLink(id); // Call contract function
+
+    if (!ipfsHash) {
+      throw new Error("No IPFS hash found");
+    }
+
+    if (!fs) {
+      throw new Error("IPFS system not initialized");
+    }
+
+    // Get content from IPFS using Helia
+    const decoder = new TextDecoder();
+    let content = "";
+    for await (const chunk of fs.cat(ipfsHash)) {
+      content += decoder.decode(chunk, { stream: true });
+    }
+
+    // Save to cache
+    cache.set(id, content);
+
+    return content;
+  } catch (error) {
+    console.error(`Error for ID ${id}:`, error);
+    throw error;
+  }
 }
 
-// Middleware to extract subdomain as rnssubdomainid
+// Middleware to extract subdomain as rnssubdomain
 app.use((req, res, next) => {
-    const host = req.headers.host;
-    const subdomain = host.split('.')[0]; // Assumes subdomain is the first part
-    req.rnssubdomainid = subdomain;
-    console.log({ subdomain })
-    next();
+  const host = req.headers.host;
+  const subdomain = host.split(".")[0]; // Assumes subdomain is the first part
+  req.rnssubdomain = subdomain;
+  next();
 });
 
 // Health check endpoint
-app.get('/health', (req, res) => {
-    res.send('OK');
+app.get("/health", (req, res) => {
+  res.send("OK");
 });
 // Main route handler
-app.get('/', async (req, res) => {
-    try {
-        const rnssubdomainid = req.rnssubdomainid;
-        
-        console.log({ host: req.headers.host });
-        // Basic validation
-        if (!rnssubdomainid || rnssubdomainid.length < 3) {
-            return res.status(400).send('Invalid subdomain ID');
-        }
+app.get("/", async (req, res) => {
+  try {
+    const rnssubdomain = req.rnssubdomain;
 
-        const content = await getIpfsContentById(rnssubdomainid);
-        res.send(content);
-
-    } catch (error) {
-        if (error.message === 'No IPFS hash found') {
-            res.status(404).send('No IPFS content found for this subdomain ID');
-        } else {
-            console.error('Server error:', error);
-            res.status(500).send('Error fetching content');
-        }
+    // Basic validation
+    if (!rnssubdomain || rnssubdomain.length < 3) {
+      return res.status(400).send("Invalid RNS");
     }
+
+    const rnsId = await getRNSUnifiedLink(`${rnssubdomain}.ron`);
+    const content = await getIpfsContentById(rnsId);
+    res.send(content);
+  } catch (error) {
+    if (error.message === "No IPFS hash found") {
+      res.status(404).send("No IPFS content found for this subdomain ID");
+    } else {
+      console.error("Server error:", error);
+      res.status(500).send("Error fetching content");
+    }
+  }
 });
 
-
-
 // Move this to the end of the file
-startServer().catch(error => {
-  console.error('Failed to start server:', error);
+startServer().catch((error) => {
+  console.error("Failed to start server:", error);
   process.exit(1);
 });
 
 // Handle errors gracefully
-process.on('uncaughtException', (error) => {
-    console.error('Uncaught Exception:', error);
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught Exception:", error);
 });
 
-process.on('unhandledRejection', (error) => {
-    console.error('Unhandled Rejection:', error);
+process.on("unhandledRejection", (error) => {
+  console.error("Unhandled Rejection:", error);
 });
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const RNSIPFSRegistry = artifacts.require("RNSIPFSRegistry");
 module.exports = async function(deployer, network, accounts) {
   console.log("Starting deployment of RNSIPFSRegistry...");
   try {
-    const defaultIpfsLink = "bafybeibcnb4bgoa46acdbhxpogpphfjut5i2ns4pcmwhql6npg4ii5os5m";
+    const defaultIpfsLink = "bafybeictek24m2xoh33zq66v4ujf7i7gpw53pxnf7rz7gvhjamjzejz3de";
     const nftContractAddress = "0x0cc07f120dffdf507a6e983c54bcb402fc6bf716";
     
     await deployer.deploy(RNSIPFSRegistry, defaultIpfsLink, nftContractAddress);
