0874baf774ce9a95ae2a6b4bb7dba99b1c076051
viko42 <vlancien.paris@gmail.com>
2024-10-24 02:26:18 +0100

    fix: update default ipfs link for owner

--- contracts/RNSIPFSRegistry.sol
+++ contracts/RNSIPFSRegistry.sol
@@ -1,16 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 interface RNSChecker {
     function getAddress(uint256 id) external view returns (address);
 }
 
-contract RNSIPFSRegistry {
+contract RNSIPFSRegistry is Ownable {
     string public defaultIpfsLink;
     mapping(string => string) private rnsLinks;
     RNSChecker public nftContract;
 
-    constructor(string memory _defaultIpfsLink, address _nftContractAddress) {
+    constructor(string memory _defaultIpfsLink, address _nftContractAddress) Ownable(msg.sender) {
         defaultIpfsLink = _defaultIpfsLink;
         nftContract = RNSChecker(_nftContractAddress);
     }
@@ -26,7 +28,7 @@ contract RNSIPFSRegistry {
         return bytes(link).length > 0 ? link : defaultIpfsLink;
     }
 
-    function updateDefaultIpfsLink(string memory _newDefaultLink) public {
+    function updateDefaultIpfsLink(string memory _newDefaultLink) public onlyOwner {
         defaultIpfsLink = _newDefaultLink;
     }
 }
